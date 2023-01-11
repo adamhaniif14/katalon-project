cd2368fd785ed888b37286f3efed9121f7010821
adam.haniif <adam.haniif@bluebirdgroup.com>
2023-01-11 11:16:22 +0700

    first commit katalon-project

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+
+
+def static "sample.Common.startAppliucation"() {
+    (new sample.Common()).startAppliucation()
+}
---
+++ Test Suites/startTest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>startTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>65a0d296-f8a0-4f33-b03e-055534a05771</testSuiteGuid>
+   <testCaseLink>
+      <guid>ccd9d04a-c485-4fe6-be43-c9b924997f55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mexican Coffee Timer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
